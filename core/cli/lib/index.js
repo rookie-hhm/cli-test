74d082986fea92e0677e5871d96a9558ffae8d23
sam <247765564@qq.com>
2020-12-19 00:33:20 +0800

    week3

---
+++ core/cli/lib/index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+module.exports = core;
+
+const path = require('path');
+const semver = require('semver');
+const colors = require('colors/safe');
+const userHome = require('user-home');
+const pathExists = require('path-exists').sync;
+const log = require('@imooc-cli-dev/log');
+
+const constant = require('./const');
+const pkg = require('../package.json');
+
+let args;
+
+async function core() {
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+    checkRoot();
+    checkUserHome();
+    checkInputArgs();
+    checkEnv();
+    await checkGlobalUpdate();
+  } catch (e) {
+    log.error(e.message);
+  }
+}
+
+async function checkGlobalUpdate() {
+  const currentVersion = pkg.version;
+  const npmName = pkg.name;
+  const { getNpmSemverVersion } = require('@imooc-cli-dev/get-npm-info');
+  const lastVersion = await getNpmSemverVersion(currentVersion, npmName);
+  if (lastVersion && semver.gt(lastVersion, currentVersion)) {
+    log.warn(colors.yellow(`请手动更新 ${npmName}，当前版本：${currentVersion}，最新版本：${lastVersion}
+                更新命令： npm install -g ${npmName}`));
+  }
+}
+
+function checkEnv() {
+  const dotenv = require('dotenv');
+  const dotenvPath = path.resolve(userHome, '.env');
+  if (pathExists(dotenvPath)) {
+    dotenv.config({
+      path: dotenvPath,
+    });
+  }
+  createDefaultConfig();
+  log.verbose('环境变量', process.env.CLI_HOME_PATH);
+}
+
+function createDefaultConfig() {
+  const cliConfig = {
+    home: userHome,
+  };
+  if (process.env.CLI_HOME) {
+    cliConfig['cliHome'] = path.join(userHome, process.env.CLI_HOME);
+  } else {
+    cliConfig['cliHome'] = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+  process.env.CLI_HOME_PATH = cliConfig.cliHome;
+}
+
+function checkInputArgs() {
+  const minimist = require('minimist');
+  args = minimist(process.argv.slice(2));
+  checkArgs();
+}
+
+function checkArgs() {
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  } else {
+    process.env.LOG_LEVEL = 'info';
+  }
+  log.level = process.env.LOG_LEVEL;
+}
+
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red('当前登录用户主目录不存在！'));
+  }
+}
+
+function checkRoot() {
+  const rootCheck = require('root-check');
+  rootCheck();
+}
+
+function checkNodeVersion() {
+  const currentVersion = process.version;
+  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(colors.red(`imooc-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`));
+  }
+}
+
+function checkPkgVersion() {
+  log.info('cli', pkg.version);
+}
